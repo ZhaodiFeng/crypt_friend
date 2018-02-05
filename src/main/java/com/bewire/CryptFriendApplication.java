b5edb9c5a5d74188b277016ed85fbbb13406b64e
ZhaodiFeng <fengzhaodi1993@gmail.com>
2018-02-05 11:38:40 +0100

    Init project

---
+++ src/main/java/com/bewire/CryptFriendApplication.java
@@ -0,0 +1,12 @@
+package com.bewire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptFriendApplication.class, args);
+	}
+}
